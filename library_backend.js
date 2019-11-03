ba1e190918cee50f55b77a348b072887d4f2931c
Tuomas Alanen <tuomas@paaryna.fi>
2019-11-03 21:30:20 +0200

    8.7

--- library_backend.js
+++ library_backend.js
@@ -115,6 +115,10 @@ const typeDefs = gql`
       author: String!
       genres: [String]
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -149,6 +153,17 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(p => p.name === args.name)
+      if(!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo}
+      authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
+      return updatedAuthor
+
     }
   }
 }
